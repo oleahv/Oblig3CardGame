31b13319a4b050cd6366cf4ab9fca81aa4b59c3c
Ole Anders Hvalsmarken <oleahv@stud.ntnu.no>
2021-03-31 15:57:11 +0200

    A simple GUI implemented (note that it does not show what it should to make the application work).

--- src/main/java/Backend/GameMain.java
+++ src/main/java/Backend/GameMain.java
@@ -12,22 +12,45 @@ public class GameMain extends Application{
 
 
     @Override
-    public void start(Stage Stage) throws Exception {
-
+    public void start(Stage primaryStage){
         BorderPane rootNode = new BorderPane();
-
         GridPane centerPane = new GridPane();
 
-        centerPane.add(new Button("Button A"), 1, 1);
+        centerPane.add(new Button("Button A"), 13, 13);
+        centerPane.add(new Button("Button A"), 12, 12);
 
-        rootNode.setTop(new Button("Button TestTop"));
+        rootNode.setBottom(new Button("Button TestBot"));
+        // NOTE! Can change where centerPane shows from here.
+        rootNode.setTop(centerPane);
 
-        Scene scene = new Scene(rootNode, 300, 300);
 
 
-        Stage.setTitle("BorderPane Test");
-        Stage.setScene(scene);
-        Stage.show();
 
+
+        Scene scene = new Scene(rootNode, 1250, 750);
+
+
+
+
+
+        // what should it show
+      //  Scene scene2 = new Scene(centerPane, 300, 250);
+
+
+
+
+        primaryStage.setTitle("BorderPane Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+    @Override
+    public void stop()
+    {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module Oblig3CardGame1 {
+
     requires javafx.graphics;
     requires javafx.controls;
+    exports Backend;
 }
 
